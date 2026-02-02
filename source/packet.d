d960ea9a4813278cf9c2f02c2794e1431c79cf14
BarracudaLake (aleph) <cuda@barralake.de>
2026-02-02 17:24:59 +0100

    fucky fuck fuck
    Signed-off-by: aleph (cuda) <cuda@barralake.de>

--- source/packet.d
+++ source/packet.d
@@ -1,6 +1,15 @@
-struct Packet(int ID)
+enum PACKAGE_BYTES : uint {
+	Error      = 0x80000000,
+	Processing = 0x40000000,
+	Registry   = 0x20000000
+	// Encryption?
+}
+
+//! server packets
+
+struct Packet(uint ID)
 {
-	int type = ID;
+	uint type = ID;
 }
 
 struct Packet0Handshake // dab me up
@@ -12,3 +21,17 @@ struct Packet1SexPack // this may be a mistake
 {
 	Packet!(1) p;
 }
+
+//! end server packets
+//! registry packets
+
+struct PacketR0Get {
+	Packet!(PACKAGE_BYTES.Registry | 0) p;
+}
+
+struct PacketR1Status {
+	Packet!(PACKAGE_BYTES.Registry | 1) p;
+}
+
+//! end registry packets
+
